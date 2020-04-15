bb0b14938e604452fc6ee3ba39a34e3f1f431a16
Bene Imre <beneimi99@gmail.com>
2020-04-15 20:29:56 +0200

    Completed GetPrime() method

---
+++ src/encrypt/EncryptMath.java
@@ -0,0 +1,77 @@
+package encrypt;
+
+import java.math.BigInteger;
+
+public class EncryptMath {
+
+    public static BigInteger ModPow(BigInteger base, BigInteger power, BigInteger mod){
+
+        BigInteger result = new BigInteger("1");
+
+        BigInteger[] mods = new BigInteger[power.bitLength()];
+        mods[0] = base.mod(mod);
+
+        for (int i = 1; i < power.bitLength(); i++){
+            mods[i] = mods[i-1].pow(2).mod(mod);
+        }
+
+        for (int i = 0; i < power.bitLength(); i++){
+            if (power.testBit(i)){
+                result = result.multiply(mods[i]);
+            }
+        }
+
+        return result.mod(mod);
+    }
+
+
+    public static BigInteger ExtEuclidean(BigInteger m, BigInteger n){
+        BigInteger a;
+        BigInteger b;
+        if(m.compareTo(n) == 1) {
+            a = m;
+            b = n;
+        }
+        else {
+            b = m;
+            a=n;
+        }
+        BigInteger q;
+        BigInteger r = new BigInteger("1");
+        BigInteger gcd = new BigInteger("1");
+        BigInteger s0 = new BigInteger("1");
+        BigInteger s1 = new BigInteger("0");
+        BigInteger t0 = new BigInteger("0");
+        BigInteger t1 = new BigInteger("1");
+
+        BigInteger buffer;
+
+        while (!r.equals(BigInteger.ZERO)){
+            gcd = r;
+            q = a.divide(b);
+            r = a.subtract(b.multiply(q));
+            a = b;
+            b = r;
+            buffer = s1;
+            s1 = s0.subtract(q.multiply(s1));
+            s0 = buffer;
+
+            buffer = t1;
+            t1 = t0.subtract(q.multiply(t1));
+            t0 = buffer;
+
+        }
+
+        return t0;
+    }
+
+    public static BigInteger Pow(BigInteger base, BigInteger pow){
+
+        while (!pow.equals(BigInteger.ZERO)){
+            base = base.multiply(base);
+            pow.subtract(BigInteger.ONE);
+        }
+        return base;
+    }
+
+}
--- src/encrypt/PrimeGenerator.java
+++ src/encrypt/PrimeGenerator.java
@@ -2,13 +2,82 @@ package encrypt;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Random;
 
 public class PrimeGenerator {
 
-    public static BigDecimal mills = new BigDecimal("1.3063778838630806904686144926");
+
+    public static BigInteger GetPrime(int bits){
+        BigInteger p;
+        BigInteger d;
+        BigInteger s;
+        BigInteger a;
+
+        while (true){
+            p = RandomBitsOf(bits);
+            d = p.subtract(BigInteger.ONE);
+            s = new BigInteger("1");
 
 
-    public static BigInteger GetMillsPrime(int n){
-        return mills.pow((int)Math.pow(3,n)).toBigInteger();
+            while (!d.mod(BigInteger.TWO).equals(BigInteger.ONE)){
+                d = d.divide(BigInteger.TWO);
+                s = s.multiply(BigInteger.TWO);
+            }
+            s = s.divide(BigInteger.TWO);
+            // r and s calculated
+
+
+            if (Test(p,d,s)){
+                break;
+            }
+        }
+
+        return p;
+    }
+
+    public static BigInteger RandomBitsOf(int bit) {
+
+        BigInteger n = new BigInteger(bit,new Random());
+        if (n.testBit(0)){
+            n.subtract(BigInteger.ONE);
+        }
+        return n;
+    }
+
+    public static BigInteger RandomTo(BigInteger to){
+
+        Random rand = new Random();
+
+        BigDecimal t = new BigDecimal(to);
+
+        BigDecimal d = new BigDecimal(rand.nextDouble()).multiply(t.subtract(BigDecimal.ONE).add(BigDecimal.ONE));
+
+        return d.toBigInteger();
+    }
+
+    public static boolean Test(BigInteger n, BigInteger d, BigInteger s){
+
+        BigInteger a;
+
+        boolean prime = false;
+        for (int k = 0; k<4; k++){
+            a = RandomTo(n.subtract(BigInteger.ONE));
+
+            if(EncryptMath.ModPow(a,d,n).equals(BigInteger.ONE)){
+                prime = true;
+            }
+            for (int i = 0; i < s.intValue(); i++){
+
+                if(EncryptMath.ModPow(a, BigInteger.TWO.pow(i).multiply(d), n ).equals(n.subtract(BigInteger.ONE))){
+                    prime = true;
+                    i = s.intValue();
+                }
+            }
+            if (!prime){
+                return false;
+            }
+            prime = false;
+        }
+        return true;
     }
 }
